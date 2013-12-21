296bf45bb02be1218927b47bda8bfffe32c3516e
Sébastien Baudin <sebastien.baudin@hefr.ch>
2013-12-21 21:17:30 +0100

    Add admin panel to manage the news

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from news.models import News
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display   = ('title', 'author', 'pub_date')
+    list_filter    = ('author', )
+    ordering       = ('pub_date', )
+    search_fields  = ('title', 'text')
+
+admin.site.register(News, NewsAdmin)
 
 # Register your models here.

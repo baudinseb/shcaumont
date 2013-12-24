153a83c9f8dffa6047237fb4e649e794146631e4
Sébastien Baudin <sebastien.baudin@hefr.ch>
2013-12-24 14:49:27 +0100

    Update the standings view to manage the standings by Aumont Team and not by Division

--- team/urls.py
+++ team/urls.py
@@ -4,6 +4,7 @@ from team import views
 
 urlpatterns = patterns('',
     url(r'^$', views.standings, name='standings'),
+    url(r'^standings/(\d+)/$', views.standings_team, name='standings_team'),
     url(r'^teams/', views.teams, name='teams'),
     
 )
--- team/views.py
+++ team/views.py
@@ -5,8 +5,18 @@ from team.models import Team, Division, Season
 
 def standings(request):
     season = Season.objects.filter(current=True).get()
-    teams = season.team_set.extra(select={"points": "wins * 2 + draws"}).order_by('points').reverse().all()
-    return render(request, 'team/standings.html', {'teams':teams})
+    aumont_teams = season.team_set.filter(name__startswith='SHC Aumont').all()   
+    id_division = aumont_teams[0].division.id
+    teams = season.team_set.filter(division=id_division).extra(select={"points": "wins * 2 + draws"}).order_by('points').reverse().all()
+    return render(request, 'team/standings.html', {'aumont_teams':aumont_teams, 'teams': teams, 'my_team': aumont_teams[0]})
+
+def standings_team(request, id_team):
+    season = Season.objects.filter(current=True).get()
+    aumont_teams = season.team_set.filter(name__startswith='SHC Aumont').all()    
+    my_team = Team.objects.filter(id=id_team).get()
+    
+    teams = season.team_set.filter(division=my_team.division.id).extra(select={"points": "wins * 2 + draws"}).order_by('points').reverse().all()
+    return render(request, 'team/standings.html', {'aumont_teams':aumont_teams, 'teams': teams, 'my_team': my_team})
 
 def teams(request):
     season = Season.objects.filter(current=True).get()

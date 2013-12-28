964bdff46e69dc64ee993a5de54d4c3abd2d76eb
Sébastien Baudin <sebastien.baudin@hefr.ch>
2013-12-28 07:55:35 +0100

    General update for the look and feel

--- team/urls.py
+++ team/urls.py
@@ -3,8 +3,7 @@ from django.conf.urls import patterns, url
 from team import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.standings, name='standings'),
-    url(r'^standings/(\d+)/$', views.standings_team, name='standings_team'),
-    url(r'^teams/', views.teams, name='teams'),
+    url(r'^$', views.teams, name='teams'),
+    url(r'^(\d+)/$', views.teams_team, name='teams'),
     
 )
--- team/views.py
+++ team/views.py
@@ -2,22 +2,13 @@
 from django.shortcuts import render
 from team.models import Team, Division, Season
 
-def standings(request):
-    season = Season.objects.filter(current=True).get()
-    aumont_teams = season.team_set.filter(name__startswith='SHC Aumont').all()   
-    id_division = aumont_teams[0].division.id
-    teams = season.team_set.filter(division=id_division).extra(select={"points": "wins * 2 + draws"}).order_by('points').reverse().all()
-    return render(request, 'team/standings.html', {'aumont_teams':aumont_teams, 'teams': teams, 'my_team': aumont_teams[0]})
-
-def standings_team(request, id_team):
-    season = Season.objects.filter(current=True).get()
-    aumont_teams = season.team_set.filter(name__startswith='SHC Aumont').all()    
-    my_team = Team.objects.filter(id=id_team).get()
-    
-    teams = season.team_set.filter(division=my_team.division.id).extra(select={"points": "wins * 2 + draws"}).order_by('points').reverse().all()
-    return render(request, 'team/standings.html', {'aumont_teams':aumont_teams, 'teams': teams, 'my_team': my_team})
-
 def teams(request):
     season = Season.objects.filter(current=True).get()
     teams = season.team_set.filter(name__startswith='SHC Aumont').all()
-    return render(request, 'team/teams.html', {'teams':teams})
+    return render(request, 'team/teams.html', {'teams':teams, 'my_team': teams[0]})
+
+def teams_team(request, id_team):
+    season = Season.objects.filter(current=True).get()
+    teams = season.team_set.filter(name__startswith='SHC Aumont').all()
+    my_team = Team.objects.filter(id=id_team).get()
+    return render(request, 'team/teams.html', {'teams':teams, 'my_team': my_team})
